9d60ebae8a8291f8b3373ecf23cd59f4004add62
Jibrail0398 <izamikatsuka@gmail.com>
2026-01-06 11:03:48 +0700

    add hud.tscn

--- Script/node.gd
+++ Script/node.gd
@@ -3,7 +3,9 @@ extends Node
 var score = 0
 
 @onready var coin_label: Label = $Coin
+@onready var hud_scene = %HUD
 
 func add_poin():
 	score += 1
 	coin_label.text = "You Collected: " + str(score)
+	hud_scene.update_score_text(score)
--- hud.gd
+++ hud.gd
@@ -1,1 +1,7 @@
 extends CanvasLayer
+
+@onready var score_label: Label = $ScoreLabel
+
+func update_score_text(score_baru):
+	score_label.text = "Score: " + str(score_baru)
+	
